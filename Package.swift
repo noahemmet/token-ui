bd1a77491badfbbf5130aa6f70e9a7277dd1799d
Noah Emmet <noah@noahemmet.com>
2019-09-14 19:01:06 -0700

    pkg

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/noahemmet/CommonUI", .branch("spm")),
-		 .package(url: "https://github.com/noahemmet/Common", .branch("spm")),
+         .package(url: "https://github.com/noahemmet/CommonUI", .branch("master")),
+		 .package(url: "https://github.com/noahemmet/Common", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
